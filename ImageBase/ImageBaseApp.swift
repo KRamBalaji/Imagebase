30ce7003503dd429428fba31601ece602ae17524
rambalaji <rambalajikoppula1@gmail.com>
2022-12-30 21:16:17 +0530

    initialised repository

---
+++ ImageBase/ImageBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageBaseApp.swift
+//  ImageBase
+//
+//  Created by Ram Balaji Koppula on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
